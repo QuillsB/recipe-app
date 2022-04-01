812649aa770ca0f5ea3539c3ceeb9d981d357aa9
Louis Beugnon <louis.beugnon@epitech.eu>
2022-04-01 09:19:56 +0200

    Add new screen searchScreen. Add eslint

--- babel.config.js
+++ babel.config.js
@@ -2,5 +2,21 @@ module.exports = function(api) {
   api.cache(true);
   return {
     presets: ['babel-preset-expo'],
+    plugins: [
+      [
+        'module-resolver',
+        {
+          extensions: [
+            '.js',
+            '.jsx',
+            '.android.js',
+            '.android.tsx',
+            '.ios.js',
+            '.ios.tsx'
+          ],
+          root: ['./src']
+        }
+      ]
+    ]
   };
 };
